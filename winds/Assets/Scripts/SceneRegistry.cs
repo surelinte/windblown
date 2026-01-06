9a1bd306000287b25368a84fa007f0a246a93cd1
Svetlana Bolotova <51244908+surelinte@users.noreply.github.com>
2026-01-06 22:06:49 +0400

    scenes & scripts reorganization

--- winds/Assets/Scripts/SceneRegistry.cs
+++ winds/Assets/Scripts/SceneRegistry.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public static class SceneRegistry
 {
-    public const string BootstrapScene    = "Bootstrap";
-    public const string PersistentScene   = "Persistent";
-    public const string UI_GlobalScene     = "UI_Global";
-    public const string MainMenuScene     = "MainMenu";
-    /*public const string Map          = "Map";
-    public const string VisualNovel  = "VisualNovel";
-    public const string Battle       = "Battle";*/
+    public const string BootstrapScene = "Bootstrap";
+    public const string PersistentScene = "Persistent";
+    public const string UI_GlobalScene = "UI_Global";
+    public const string MainMenuScene = "MainMenu";
+    public const string MapScene = "MapGeneral";
+    public const string StoryScene = "StoryGeneral";
+    public const string BattleScene = "BattleMain";
 }
